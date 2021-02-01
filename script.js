b9112d2f932e6a9d4f5d47f3c42fd3f26feedcd5
missmandy222 <mandyerndt@gmail.com>
2021-02-01 00:29:29 -0500

    changed script

--- script.js
+++ script.js
@@ -1,38 +1,16 @@
-function formatDate(date) {
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-  let dayIndex = date.getDay();
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-  let day = days[dayIndex];
 
-  return `${day} ${hours}:${minutes}`;
-}
 
-let dateElement = document.querySelector("#date");
-let currentTime = new Date();
+//search
 
 //search
 function displayWeatherCondition(response) {
-  console.log(response.data);
+  let iconElement = document.querySelector("#icon");
+
+
   document.querySelector("#current-city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
   );
-  let iconElement = document.querySelector("#icon");
   document.querySelector("#humidity").innerHTML =
     response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
